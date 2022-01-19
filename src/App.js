3378cbf39c9af39e6100e32d477f80e55392790f
YashsviG <girdhar.yashsvi@gmail.com>
2022-01-18 20:45:02 -0800

    Calling movies from API, setting movie state, MovieList component added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './MovieList';
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([
+  ]);
+
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=star wars&apikey=2149ae03"
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    console.log("responseJson", responseJson);
+    setMovies(responseJson.Search);
+  }
+
+  useEffect(() => {
+    getMovieRequest();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid movie-app'>
+      <div className='row'>
+        <MovieList movies = {movies}/>
+      </div>
     </div>
   );
 }
-
 export default App;
